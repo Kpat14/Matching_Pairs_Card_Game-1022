90693ade7a9242ed042382b2cb7761ad2634c8e8
Kakshil Patel <Kakshil.14@gmail.com>
2022-03-18 15:36:14 -0400

    Made Username page and linked it

--- app/src/main/java/ca/yorku/cardrush/MainActivity.java
+++ app/src/main/java/ca/yorku/cardrush/MainActivity.java
@@ -2,13 +2,31 @@ package ca.yorku.cardrush;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.QuickContactBadge;
 
 public class MainActivity extends AppCompatActivity {
+    private android.widget.Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.play);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openUsername();
+            }
+        });
+    }
+
+    public void openUsername(){
+        Intent intent = new Intent(this, Username.class);
+        startActivity(intent);
     }
 }
