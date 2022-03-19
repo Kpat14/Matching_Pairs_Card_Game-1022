8908e5baf20730a2d47ece37af7cc3563ed38061
Kpat <Kakshil.14@gmail.com>
2022-03-18 21:14:39 -0400

    added main game page

--- app/src/main/java/ca/yorku/cardrush/Username.java
+++ app/src/main/java/ca/yorku/cardrush/Username.java
@@ -2,13 +2,26 @@ package ca.yorku.cardrush;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Username extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_username);
+        button = (Button) findViewById(R.id.button6);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Username.this, game.class);
+                startActivity(intent);
+            }
+        });
     }
 }
