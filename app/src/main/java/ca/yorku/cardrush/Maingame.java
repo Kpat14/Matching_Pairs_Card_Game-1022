26df0d1539432c7c2581458c7304f8448ba1d564
Kpat14 <Kakshil.14@gmail.com>
2022-03-19 01:33:03 -0400

    update

---
+++ app/src/main/java/ca/yorku/cardrush/Maingame.java
@@ -0,0 +1,14 @@
+package ca.yorku.cardrush;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Maingame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maingame);
+    }
+}
