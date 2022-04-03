51a8e9f88a845808c6adbb99f5352cc382f1ec28
Kpat14 <Kakshil.14@gmail.com>
2022-04-02 22:00:10 -0400

    Leaderboard

--- app/src/main/java/ca/yorku/cardrush/Username.java
+++ app/src/main/java/ca/yorku/cardrush/Username.java
@@ -6,23 +6,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Username extends AppCompatActivity {
     private Button button;
+    private EditText username;
+    private static String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_username);
         button = (Button) findViewById(R.id.button6);
-
+        username = (EditText) findViewById(R.id.textInputEditText);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Username.this, game.class);
                 startActivity(intent);
+                name = username.getText().toString();
             }
         });
 
+    }
+    public static String getMyString() {
+        return name;
     }
 }
--- app/src/main/java/ca/yorku/cardrush/leaderboard.java
+++ app/src/main/java/ca/yorku/cardrush/leaderboard.java
@@ -6,16 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class leaderboard extends AppCompatActivity {
 
     private Button button;
+    Double points = game.getMyString();
+    int i = Integer.valueOf(points.intValue());
+
+    String username = Username.getMyString();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
 
         button = (Button) findViewById(R.id.button11);
+        TextView textView = findViewById(R.id.textView8);
+        textView.setText(String.valueOf(i));
+
+        TextView textView2 = findViewById(R.id.textView7);
+        textView2.setText(username);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
